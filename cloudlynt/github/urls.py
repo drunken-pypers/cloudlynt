27eb499d634d7ca774314421b9e92fe1ef3338dc
Ravi Shanker B <ravi@aplopio.com>
2014-12-22 18:29:23 +0530

    Bug fix - Supports repo names with '.'

--- cloudlynt/github/urls.py
+++ cloudlynt/github/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns(
     '',
     url(
-        r'^(?P<username>[A-Za-z0-9\-]+)/(?P<repo>[A-Za-z0-9\-]+)',
+        r'^(?P<username>[A-Za-z0-9\-]+)/(?P<repo>[A-Za-z0-9\-\.]+)',
         'cloudlynt.github.views.build_lynt', name="build-github-lynt"),
 )
